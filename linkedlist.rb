b6241b547213d11a748f295f5559dc27e1057a2c
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-05-02 16:38:59 +0100

    Complete linkedlist class
    Add the remove and add_at methods

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,25 +1,26 @@
 # Start with your code from last challenge.
-
 class Node
   attr_accessor :value, :next_node
-  
+
   def initialize(value, next_node = nil)
-	  @value = value
+    @value = value
     @next_node = next_node
   end
 end
 
+# Linkedlist
 class LinkedList
-  #setup head and tail
+  # setup head and tail
   def initialize
     @head = nil
     @tail = nil
   end
-  
+
   def find_tail
     node = @head
-    return node if !node.next_node
-    return node if !node.next_node while (node = node.next_node)
+    return node unless node.next_node
+
+    return node unless node.next_node while (node = node.next_node)
   end
 
   def add(number)
@@ -34,80 +35,65 @@ class LinkedList
   def add_at(index, item)
     temp = @head
     new_node = Node.new(item)
-    if temp == nil
-      temp = new_node
+    if @head.nil?
+      @head = new_node
       return
     end
 
-    if index == 0
-     store_next = temp
-     @head = new_node
-     new_node.next_node = store_next
-     return
+    if index.zero?
+      store_next = temp
+      @head = new_node
+      new_node.next_node = store_next
+      return
     end
 
-    while(index > 1)
+    while index > 1
       temp = temp.next_node
-      break if temp.next_node == nil
-        index -= 1
+      break if temp.next_node.nil?
+
+      index -= 1
     end
-      
-    return if temp == nil
+
+    return if temp.nil?
 
     store_next = temp.next_node
     temp.next_node = new_node
-    new_node.next_node = store_next
-
+    temp.next_node.next_node = store_next
   end
 
   def remove(index)
     temp = @head
-    return if temp == nil
+    return if temp.nil?
+
     if index == 0
       @head = temp.next_node
       return
     end
 
-    while(index > 0)
+    while index > 1
       temp = temp.next_node
-      break if temp == nil
-        index -= 1
+      break if temp.nil?
+
+      index -= 1
     end
-      
-    return if temp == nil or temp.next_node == nil
 
-      store_next = temp.next_node.next_node
+    return if temp.nil? || temp.next_node.nil?
 
-      temp.next = nil
-      temp.next = store_next
+    store_next = temp.next_node.next_node
 
+    temp.next_node = nil
+    temp.next_node = store_next
   end
 
   def get(index)
     # your code here
     node = @head
     count = 0
-    while (node)
+    while node
       return node.value if count == index
-       count += 1
-       node = node.next_node
+
+      count += 1
+      node = node.next_node
     end
   end
 end
-
-list = LinkedList.new
-
-list.add(3)
-p list
-list.add(5)
-p list
-list.add_at(1, 11)
-p list
-list.add_at(0, 13)
-p list
-
-puts list.get(2)
-# => 11
-
-puts list.get(3)
-# => 5
