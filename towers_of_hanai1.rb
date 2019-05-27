16e0a5862cbaef0b74cd4a281bc88e65157b316c
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-05-27 21:27:15 +0100

    Add Towers of Hanoi - Part 1 improved solution

--- towers_of_hanai1.rb
+++ towers_of_hanai1.rb
@@ -1,22 +1,16 @@
 def move(starting, goal)
-    aux = case starting + goal
-          when 3; 3
-          when 4; 2
-          else 1
-          end
-  
-    "#{starting}->#{aux} #{starting}->#{goal} #{aux}->#{goal}"
-  end
-  
-  puts move(1, 3)
-  # => 1->2 1->3 2->3
-  
-  puts move(2, 3)
-  # => 2->1 2->3 1->3
-  
-  puts move(2, 1)
-  # => "2->3 2->1 3->1"
-  
-  puts move(1, 2)
-  # => "1->3 1->2 3->2"
-  
+  aux = (1..3).find { |n| n != starting && n != goal }
+  "#{starting}->#{aux} #{starting}->#{goal} #{aux}->#{goal}"
+end
+
+puts move(1, 3)
+# => 1->2 1->3 2->3
+
+puts move(2, 3)
+# => 2->1 2->3 1->3
+
+puts move(2, 1)
+# => "2->3 2->1 3->1"
+
+puts move(1, 2)
+# => "1->3 1->2 3->2"
