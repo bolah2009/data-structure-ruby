ea30829d983a5b28be9c0f280ad7a52a15cd06a5
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-06-10 17:40:19 +0100

    Add graph cycle solution and test

--- lib/graph_cycle.rb
+++ lib/graph_cycle.rb
@@ -1,54 +1,20 @@
 # frozen_string_literal: true
 
 def graph_cycle?(graph)
-  # write your code here
-  cycle = 0
-  i = 0
-  while i < graph.size
-    cycle += 1 if graph[i].size > 1
+  stack = [0]
+  visited = []
 
-    i += 1
- end
+  until stack.empty?
+    head = stack.pop
+    return true if visited.size > 2 && (visited.include? head)
 
-  cycle > 2
+    return false if graph[head].nil?
+
+    visited << head
+    not_visited = graph[head].reject { |node| visited.include? node }
+
+    stack += not_visited.reverse
+  end
+
+  false
 end
-
-depth_first_search
-
-puts depth_first_search(
-  0 => [2],
-  1 => [4],
-  2 => [0, 5, 3],
-  3 => [5, 2],
-  4 => [5, 1],
-  5 => [4, 2, 3]
-)
-# => true
-
-puts depth_first_search(
-  0 => [2],
-  1 => [2],
-  2 => [0, 1, 3, 4, 5],
-  3 => [2],
-  4 => [2]
-)
-# => false
-
-puts graph_cycle?(
-  0 => [2],
-  1 => [4],
-  2 => [0, 5, 3],
-  3 => [5, 2],
-  4 => [5, 1],
-  5 => [4, 2, 3]
-)
-# => true
-
-puts graph_cycle?(
-  0 => [2],
-  1 => [2],
-  2 => [0, 1, 3, 4, 5],
-  3 => [2],
-  4 => [2]
-)
-# => false
