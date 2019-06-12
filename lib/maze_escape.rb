d67d6f38047062a795b17fb408a9591e6ef5e631
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-06-12 18:29:31 +0100

    Complete maze escape method

--- lib/maze_escape.rb
+++ lib/maze_escape.rb
@@ -1,106 +1,14 @@
 # frozen_string_literal: true
 
 def maze_escape(maze)
-  # write your code here
-  path = []
-  move(maze, path, [0, 0])
-  path
+  move(maze, [], 0, 0)
 end
 
-def move(map, path, position)
-  return nil if not_bounded?(position, map.size)
+def move(map, path, row, col)
+  return nil if [row, col].any? { |i| i >= map.size } || map[row][col] == 1
 
-  y = position[0]
-  x = position[1]
-  return false if map[x][y] == 1
+  path << [col, row]
+  return path if map[row][col] == 9
 
-  path << position
-  return true if map[x][y] == 9
-
-  move(map, path, [x + 1, y]) || move(map, path, [x, y + 1])
+  move(map, path.dup, row + 1, col) || move(map, path.dup, row, col + 1)
 end
-
-def not_bounded?(position, size)
-  position.any? { |i| i.negative? || i >= size }
-end
-
-# 5x5_maze
-p maze_escape(
-  [
-    [0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 1],
-    [0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 9]
-  ]
-)
-# => [
-#      [0, 0],
-#      [1, 0],
-#      [2, 0],
-#      [2, 1],
-#      [2, 2],
-#      [2, 3],
-#      [3, 3],
-#      [4, 3],
-#      [4, 4]
-#    ]
-
-# 3x3_maze
-p maze_escape(
-  [
-    [0, 0, 0],
-    [0, 1, 9],
-    [0, 0, 0]
-  ]
-)
-
-# => [
-#   [0, 0],
-#   [1, 0],
-#   [2, 0],
-#   [2, 1]
-# ]
-
-# 7x7_maze
-p maze_escape(
-  [
-    [0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1],
-    [0, 0, 0, 0, 0, 0, 9],
-    [0, 1, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0]
-  ]
-)
-
-# => [
-#   [0, 0], [0, 1],
-#   [0, 2], [0, 3],
-#   [1, 3], [2, 3],
-#   [3, 3], [4, 3],
-#   [5, 3], [6, 3]
-# ]
-
-# 9x9_maze
-p maze_escape(
-  [
-    [0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 1, 0, 1, 0, 1, 1, 0, 1],
-    [0, 1, 0, 1, 0, 0, 0, 0, 0],
-    [1, 1, 0, 1, 1, 1, 0, 1, 1],
-    [0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 1, 1, 1, 0, 1, 1, 1, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 1, 1, 0, 0, 1, 1, 0, 9],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0]
-  ]
-)
-
-# => [
-#   [0, 0], [1, 0], [2, 0], [2, 1],
-#   [2, 2], [2, 3], [2, 4], [3, 4],
-#   [4, 4], [4, 5], [4, 6], [5, 6],
-#   [6, 6], [7, 6], [7, 7], [8, 7]
-# ]
