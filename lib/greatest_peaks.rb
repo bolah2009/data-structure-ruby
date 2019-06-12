ba7d8b058ae8b6237c21765f0a3ea94f2db84905
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-06-12 04:05:56 +0100

    Fix linter errors

--- lib/greatest_peaks.rb
+++ lib/greatest_peaks.rb
@@ -14,96 +14,69 @@ def greatest_peaks(map)
   counts.minmax
 end
 
-def search(map, col, row)
-  return nil if row >= map.size || col >= map.size
-
-  max = map.flatten.select { |i| (i.is_a? Integer) }.max if max.nil?
-  return [row, col, max] if map[row][col] == max
-
-  search(map, row + 1, col) ||
-    search(map, row, col + 1)
-end
-
-def find_peaks(map, visited, i)
+def find_peaks(map, visited, pos)
   return if visited.flatten.none? { |i| (i.is_a? Integer) }
 
-  marks = ('A'..'Z').to_a
-
-  peak = search(visited, 0, 0)
-  mark_area(map, peak[0], peak[1], peak[2], visited, marks[i])
-  find_peaks(map, visited, i + 1)
+  peak = search(visited, max_peak(visited), 0, 0)
+  mark_area(map,  peak[0..1], peak[2], visited, ('A'..'Z').to_a[pos])
+  find_peaks(map, visited, pos + 1)
   visited
 end
 
-def mark_area(map, row, col, peak, visited, mark)
-  return nil if [col.negative?, row.negative?, (row >= map.size), (col >= map.size)].any? || [(visited[row][col].is_a? String)].any?
-  return if peak < map[row][col]
-
-  # p [(col - 1), (col + 1), (row - 1), (row + 1)]
-  sides = [(!(row - 1).negative? && (peak < map[row - 1][col])),
-           (!(col - 1).negative? && (peak < map[row][col - 1])),
-           ((row + 1) < map.size && (peak < map[row + 1][col])),
-           ((col + 1) < map.size && (peak < map[row][col + 1]))]
-
-  return if sides.any?
-
-  visited[row][col] = mark
-  mark_area(map, row, col - 1, map[row][col], visited, mark) ||
-    mark_area(map, row, col + 1, map[row][col], visited, mark) ||
-    mark_area(map, row - 1, col, map[row][col], visited, mark) ||
-    mark_area(map, row + 1, col, map[row][col], visited, mark)
+def max_peak(map)
+  map.flatten.select { |i| (i.is_a? Integer) }.max
 end
 
-p greatest_peaks(
-  [
-    [9, 8, 5],
-    [5, 6, 3],
-    [8, 4, 1]
-  ]
-)
-# => [3, 6]
+def search(map, max, col, row)
+  return nil if row >= map.size || col >= map.size
 
-p greatest_peaks(
-  [
-    [8, 12],
-    [9, 3]
-  ]
-)
-# => [1, 3]
+  return [row, col, max] if map[row][col] == max
 
-# first_4x4_altitud_map
-p greatest_peaks(
-  [
-    [2, 6, 9, 11],
-    [7, 8, 9, 8],
-    [6, 7, 12, 9],
-    [10, 7, 6, 4]
-  ]
-)
+  search(map, max, row + 1, col) || search(map, max, row, col + 1)
+end
 
-# => [3, 9]
+def mark_area(map, position, peak, visited, mark)
+  return nil if not_bounded?(position, map.size)
 
-# second_4x4_altitude_map
-p greatest_peaks(
-  [
-    [4, 3, 2, 1],
-    [2, 1, 0, 5],
-    [0, 7, 4, 6],
-    [10, 8, 4, 7]
-  ]
-)
+  row = position[0]
+  col = position[1]
+  return if (visited[row][col].is_a? String) ||
+            invalid_area?(map, peak, row, col)
 
-# => [4, 7]
+  visited[row][col] = mark
+  mark_sides(map, row, col, visited, mark)
+end
 
-# 5x5_altitude_map
-p greatest_peaks(
-  [
-    [3, 6, 9, 11, 3],
-    [5, 8, 12, 4, 6],
-    [7, 10, 13, 2, 14],
-    [0, 1, 5, 9, 3],
-    [2, 8, 6, 4, 0]
-  ]
-)
+def not_bounded?(position, size)
+  position.any? { |i| i.negative? || i >= size }
+end
 
-# => [2, 13]
+def invalid_area?(map, peak, row, col)
+  peak < map[row][col] ||
+    [check_col(map, peak, row, col), check_row(map, peak, row, col)].any?
+end
+
+def check_col(map, peak, row, col)
+  [(!(col - 1).negative? && (peak < map[row][col - 1])),
+   ((col + 1) < map.size && (peak < map[row][col + 1]))].any?
+end
+
+def check_row(map, peak, row, col)
+  [(!(row - 1).negative? && (peak < map[row - 1][col])),
+   ((row + 1) < map.size && (peak < map[row + 1][col]))].any?
+end
+
+def mark_sides(map, row, col, visited, mark)
+  check_left_right(map, row, col, visited, mark)
+  check_up_down(map, row, col, visited, mark)
+end
+
+def check_left_right(map, row, col, visited, mark)
+  mark_area(map, [row, col - 1], map[row][col], visited, mark)
+  mark_area(map, [row, col + 1], map[row][col], visited, mark)
+end
+
+def check_up_down(map, row, col, visited, mark)
+  mark_area(map, [row - 1, col], map[row][col], visited, mark)
+  mark_area(map, [row + 1, col], map[row][col], visited, mark)
+end
