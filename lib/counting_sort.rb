f7afa51b9bcd22bde85c7c324bd1e93ebce3e340
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-06-17 18:42:41 +0100

    change counting_sort method of instantiating a new array

--- lib/counting_sort.rb
+++ lib/counting_sort.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def counting_sort(array)
-  count_array = Array.new(100).fill(0)
+  count_array = Array.new(100, 0)
   array.each { |i| count_array[i] += 1 }
   count_array
 end
